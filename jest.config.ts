734e8c31ecc940017ff90a0f96700e94d8abcbda
JasrajChouhan <jasrajc945@gmail.com>
2024-10-11 04:19:56 +0530

    (conf): jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type {Config} from '@jest/types';
+
+const config : Config.InitialOptions = {
+    testEnvironment : 'node',
+    verbose : true ,
+    preset : 'ts-jest'
+}
+
+export default config ;
