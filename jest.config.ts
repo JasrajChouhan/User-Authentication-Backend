843e28f5172c7551d05833dee1df25c3d5c4319e
JasrajChouhan <jasrajc945@gmail.com>
2024-10-15 01:41:00 +0530

    (style): code format by prettier

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
 
-const config : Config.InitialOptions = {
-    testEnvironment : 'node',
-    verbose : true ,
-    preset : 'ts-jest'
-}
+const config: Config.InitialOptions = {
+  testEnvironment: 'node',
+  verbose: true,
+  preset: 'ts-jest',
+};
 
-export default config ;
+export default config;
