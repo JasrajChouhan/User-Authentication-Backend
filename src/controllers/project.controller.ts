c8652347f440efa2f9927194a6601c468b694e3a
JasrajChouhan <jasrajc945@gmail.com>
2024-12-08 20:39:43 +0530

    (feat): project controller

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,39 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import fs from 'fs/promises';
-import path from 'path';
-import { promisify } from 'node:util';
-import child_process from 'node:child_process';
-import { v4 as uuidv4 } from 'uuid';
-
+import { ProjectService } from '../services/project.service';
 import ApiResponse from '../utils/ApiResponse';
-import ApiError from '../utils/ApiError';
+
+const projectService = new ProjectService();
 
 export const createProject = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const exec = promisify(child_process.exec);
+    const { projectName, projectTechStack, description } = req.body;
+    const authorId = req.user?._id as string;
+    const projectData = { projectName, projectTechStack, description, authorId };
+    const project = await projectService.createProject(projectData);
 
-    // Navigate to the /projects directory (Ensure it exists)
-    const projectsDir = path.join(__dirname, '..', '..', 'projects');
-    await fs.mkdir(projectsDir, { recursive: true });
-
-    // Create a folder with a unique name
-    const folderName = uuidv4();
-    const projectPath = path.join(projectsDir, folderName);
-    await fs.mkdir(projectPath);
-
-    // Run Vite command to initialize a new project
-    const command = `cd ${projectPath} && npm create vite@latest codesandbox -- --template react`;
-
-    const { stdout, stderr } = await exec(command);
-
-    if (stderr) {
-      throw new ApiError(500, `Error setting up Vite project: ${stderr}`);
-    }
-
-    return res.status(200).json(new ApiResponse(200, { folderName, stdout }, 'Project created successfully.'));
+    return res.status(201).json(new ApiResponse(201, project, 'Successfully createing projects.'));
   } catch (error: any) {
-    return res
+    res
       .status(error.statusCode || 500)
-      .json(new ApiError(error.statusCode || 500, error.message || 'Internal Server Error'));
+      .json(
+        new ApiResponse(
+          error.statusCode || 500,
+          {},
+          error?.message || 'Error while creating project, Please try again letter.'
+        )
+      );
   }
 };
